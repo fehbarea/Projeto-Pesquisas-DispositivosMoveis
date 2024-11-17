d0f3d5e381c75f73f2155b87cceac14c7333cd99
fehbarea <fehbarea@icloud.com>
2024-11-17 19:23:44 -0300

    AcoesDePesquisa criada

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Calculadora from "./src/screens/Calculadora.js";
+import acoesDePesquisa from "./src/screens/AcoesDePesquisa.js"
 import Drawer from "./src/screens/Drawer.js";
 import icon from "react-native-vector-icons/MaterialIcons.js"
 
@@ -10,7 +11,7 @@ const App = () =>{
   return(
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Calculadora" screenOptions={{ headerShown: false, headerTintColor: 'blue', headerTitleStyle:{color:"white"} , headerStyle: {backgroundColor: "#BBBBBB"}  }}>
-      <Stack.Screen name="Calculadora" component={Calculadora} />
+      <Stack.Screen name="Calculadora" component={acoesDePesquisa} />
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ src/screens/AcoesDePesquisa.js
@@ -0,0 +1,72 @@
+//Importação
+
+import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput } from "react-native";
+import { useState } from "react";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+
+//Definição
+
+const AcoesDePesquisa = (props) => {
+
+
+
+    return (
+        <View style={estilos.view}>
+
+            <View style={estilos.containerBotoes}>
+                <TouchableOpacity style={estilos.botao}>
+                    <Icon name='file-document-edit-outline' size={60} style={estilos.iconBotao}/>
+                    <Text style={estilos.textoBotao}>Modificar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={estilos.botao} >
+                <Icon name='checkbox-multiple-marked-outline' size={60} style={estilos.iconBotao}/>
+                    <Text style={estilos.textoBotao}>Coletar dados</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={estilos.botao} >
+                <Icon name='chart-donut' size={60} style={estilos.iconBotao}/>
+                    <Text style={estilos.textoBotao}>Relatório</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    );
+}
+
+const estilos = StyleSheet.create({
+    view:{
+        backgroundColor: '#372775',
+        padding: '5%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    containerBotoes: {
+        
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: "center",
+        width: '100%'
+    },
+    botao: {
+        gap:2,
+        backgroundColor: '#312464',
+        height: 100,
+        width: 100,
+        padding: 10,
+    },
+    textoBotao:{
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 12.5,
+        fontFamily: 'AveriaLibre-Regular'
+    },
+    iconBotao:{
+        color: 'white',
+        textAlign: 'center',
+    }
+
+})
+
+//Exportação
+
+export default AcoesDePesquisa;
