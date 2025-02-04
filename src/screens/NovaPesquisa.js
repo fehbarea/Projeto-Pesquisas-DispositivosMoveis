35c882e8bd904b7ae7e8e46db8d6da31f28435e9
Daniel Jacob <danieljaroszczuk28@gmail.com>
2025-02-03 23:31:03 -0300

    Melhorada a função de selecionar imagem em NovaPesquisa

--- src/screens/NovaPesquisa.js
+++ src/screens/NovaPesquisa.js
@@ -4,8 +4,10 @@ import {View, Text, Image, StyleSheet, TouchableOpacity,TextInput, Pressable} fr
 import {useState} from "react";
 import Card from "../components/Card";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons"
-import { launchImageLibrary } from "react-native-image-picker";
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import ImageResizer from "react-native-image-resizer";
+import { Alert } from 'react-native';
+import { PermissionsAndroid } from 'react-native';
 
 
 //Definição
@@ -29,6 +31,21 @@ const NovaPesquisa = (props) => {
 
   }
 
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: 'Permissão para usar a Câmera',
+          message: 'O app precisa de permissão para acessar a câmera',
+          buttonPositive: 'OK',
+        }
+      );
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
   const convertUriToBase64 = async (uri) => {
     const resizedImage = await ImageResizer.createResizedImage(
       uri,
@@ -49,10 +66,43 @@ const NovaPesquisa = (props) => {
 
 
   const obterImagem = () =>{
-    launchImageLibrary({mediaType: "photo"},(result) => {
-      convertUriToBase64(result.assets[0].uri);
-    })
-  }
+    Alert.alert(
+      'Escolha a origem da imagem',
+      '',
+      [
+        {
+          text: 'Galeria',
+          onPress: () => {
+            launchImageLibrary({ mediaType: 'photo' }, (result) => {
+              if (result.assets && result.assets.length > 0) {
+                convertUriToBase64(result.assets[0].uri);
+              } else {
+                console.log('Nenhuma imagem selecionada');
+              }
+            });
+          },
+        },
+        {
+          text: 'Câmera',
+          onPress: () => {
+            requestCameraPermission();
+            launchCamera({ mediaType: 'photo' }, (result) => {
+              if (result.assets && result.assets.length > 0) {
+                convertUriToBase64(result.assets[0].uri);
+              } else {
+                console.log('Nenhuma imagem tirada');
+              }
+            });
+          },
+        },
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+      ],
+      { cancelable: true }
+    );
+  };
 
   return(
     <View style={estilos.view}>
@@ -80,6 +130,7 @@ const NovaPesquisa = (props) => {
           <Pressable style={({ pressed }) => [estilos.inputImagem, { transform: [{ scale: pressed ? 0.95 : 1 }] }, ]} onPress={obterImagem}>
             <Text style={estilos.textoImagem}>Câmera/Galeria de imagens</Text>
           </Pressable>
+          <Image source={{uri:imagem}} style={estilos.imagemSelecionada} resizeMode="contain"/>
         </View>
 
 
@@ -95,7 +146,10 @@ const NovaPesquisa = (props) => {
 }
 
 const estilos = StyleSheet.create({
-
+  imagemSelecionada:{
+    height:'90%',
+    width: "30%"
+  },
   view: {
     flex: 1,
     flexDirection: "column",
@@ -132,9 +186,9 @@ const estilos = StyleSheet.create({
   cImagem: {
     width: "90%",
     flex:0.40,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
 
   cBotao: {
